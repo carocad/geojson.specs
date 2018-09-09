1900e4b690c2b4f651d3a37afa1864620fef82d2
Camilo Roca <camilo.roca@cobi.bike>
2018-09-09 17:16:53 +0200

    lint project with eastwood

--- project.clj
+++ project.clj
@@ -5,18 +5,6 @@
             :url "https://raw.githubusercontent.com/hiposfer/geojson.specs/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
-  :plugins [[jonase/eastwood "0.2.5"]
-            [lein-cljsbuild "1.1.7"]]
-  :cljsbuild ;; TODO: add tests
-  {:builds
-   [{:id "dev"
-     :source-paths ["src"]
-     :compiler {:optimizations :none
-                :pretty-print true}}
-    {:id "simple"
-     :source-paths ["src"]
-     :compiler {:optimizations :simple
-                :static-fns true}}
-    {:id "advance"
-     :source-paths ["src"]
-     :compiler {:optimizations :advanced}}]})
+  :plugins [[jonase/eastwood "0.2.9"]]
+  ;; deploy to clojars as - lein deploy releases
+  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org/repo"}]])
