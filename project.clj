d3d0a1f3729f5f389d9c37da402068143470a575
Camilo Roca <roc1fr@bosch.com>
2021-08-25 20:36:33 +0200

    chore: use example files for testing

--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
-(defproject hiposfer/geojson.specs "0.2.0"
+(defproject net.clojars.carocad/geojson.specs "0.2.0"
   :description "a Geojson spec for validating data using Clojure's spec"
-  :url "git@github.com:hiposfer/geojson.specs"
-  :license {:name "GNU LESSER GENERAL PUBLIC LICENSE"
-            :url "https://raw.githubusercontent.com/hiposfer/geojson.specs/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
-  :plugins [[jonase/eastwood "0.2.9"]]
-  :eastwood {:config-files ["resources/eastwood.clj"]}
+  :url "https://github.com/carocad/geojson.specs"
+  :license {:name "LGPL v3"
+            :url  "https://github.com/carocad/geojson.specs/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.597" :scope "provided"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0"]  ;; generative testing
+                                  [org.clojure/data.json "0.2.6"]]
+                   :plugins [[jonase/eastwood "0.2.9"]]
+                   :eastwood {:config-files ["resources/eastwood.clj"]}}}
+
   ;; deploy to clojars as - lein deploy releases
-  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org/repo"}]])
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]])
