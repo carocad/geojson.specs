a3c6bb729b2d7bd0d4b8e1013fef922a23fdaf74
Camilo Roca <roc1fr@bosch.com>
2021-08-25 20:37:23 +0200

    fix: add generative testing to the repo

--- test/hiposfer/geojson/specs_test.clj
+++
@@ -1,8 +0,0 @@
-(ns hiposfer.geojson.specs-test
-  (:require [clojure.test :as test]
-            [hiposfer.geojson.specs :as geojson]))
-
-;; TODO
-(test/deftest a-test
-  (test/testing "FIXME, I fail."
-    (test/is (= 0 1))))
